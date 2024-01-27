1357975d7e8f654e4bee05e58275e2d1d5bf7ed2
KundwaBruno <kundwabruno@gmail.com>
2024-01-27 12:36:47 +0200

    Include sinc on the projects section

--- src/components/Skeletons/feedback.tsx
+++ src/components/Skeletons/feedback.tsx
@@ -5,14 +5,14 @@ interface FeedbackProps {}
 const FeedbackSkeleton: FC<FeedbackProps> = () => {
   return (
     <div>
-      {new Array(3).fill(0).map((item, index) => {
+      {new Array(3).fill(0).map((_, index) => {
         return (
-          <div key={index} className="my-8 rounded-lg p-3">
-            <div className="flex flex-1 gap-3">
-              <div className="w-10 h-10 rounded-full bg-gray-300 dark:bg-gray-500" />
+          <div key={index} className="mb-8">
+            <div className="flex flex-1 gap-1">
+              <div className="w-12 h-12 rounded-full bg-gray-300 dark:bg-gray-500" />
               <div className="w-full flex-1">
                 <div className="w-1/4 h-2 rounded-full bg-gray-300 dark:bg-gray-500" />
-                <div className="w-1/2 h-2 rounded-full bg-gray-300 dark:bg-gray-500 my-5" />
+                <div className="w-1/2 h-2.5 rounded-full bg-gray-300 dark:bg-gray-500 my-5" />
                 <div className="flex items-center gap-2 flex-1">
                   <div className="w-4 h-2 rounded-full bg-gray-300 dark:bg-gray-500" />
                   <div className="w-4 h-2 rounded-full bg-gray-300 dark:bg-gray-500" />
